78dd7c0e2cc5ddf0868989b35bfcc70bd5b525e1
Huespal <Huespal@users.noreply.github.com>
2025-06-18 09:57:17 +0200

    feat: add Not Found page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,5 @@
 import SearchForm from '@/components/Search/Form';
 import { getLocations } from '@/domain/locations/api';
-import './styles.css';
 
 export const dynamic = 'force-dynamic';
 
@@ -8,7 +7,7 @@ export default async function Home() {
   const locations = await getLocations();
 
   return (
-    <div className="home-page">
+    <div className="page">
       <h1>Search an itinerary</h1>
       <p>Find your next destination!</p>
       <SearchForm locations={locations} />
